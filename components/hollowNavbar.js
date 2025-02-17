dcd8ec9a45ab107928ed506947036905fda37b50
Damon-Nunez <damon.j.nunez@gmail.com>
2025-02-17 18:28:00 -0500

    TroubleShooting

---
+++ components/hollowNavbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './hollowNavbar.css'
+const hollowNavbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default hollowNavbar
---
+++ pages/hollowProfile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './hollowProfile.css'
+
+const hollowProfile = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default hollowProfile
